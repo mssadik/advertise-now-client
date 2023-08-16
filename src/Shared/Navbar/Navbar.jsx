500defca7750f21259f2153d487f13c9d65fe68c
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-08-17 01:20:14 +0600

    Btn update Color

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -60,11 +60,11 @@ const Navbar = () => {
           user ?
           <>
             <img style={{height: '50px', width: '50px'}} className="rounded-full mr-2 md:mr-4" src={user?.photoURL} alt="" />
-            <Link onClick={handleSignOut}><a className="btn bg-orange-700 border-none px-6 uppercase text-white">signOut</a></Link>
+            <Link onClick={handleSignOut}><a className="btn border-orange-500 bg-orange-500 text-white hover:text-orange-500 hover:border hover:border-orange-500 hover:bg-white">signOut</a></Link>
           </> 
           :
           // <Link to="/login"><a className="btn bg-orange-700 border-none px-6 uppercase text-white">Login</a></Link>
-          <Link className="btn bg-orange-700 border-none px-6 uppercase text-white" to="/login">Login</Link>
+          <Link className="btn border-orange-500 bg-orange-500 text-white hover:text-orange-500 hover:border hover:border-orange-500 hover:bg-white" to="/login">Login</Link>
         }
       </div>
     </div>
