8451c014bc5ff0be36b6f90da74e76005b4f34d5
Sadik <anarul6777@gmail.com>
2023-08-18 12:01:25 +0600

    upadated advertisement json data

--- src/pages/Home/Advertisement/AdvertiseCard.jsx
+++ src/pages/Home/Advertisement/AdvertiseCard.jsx
@@ -1,23 +1,23 @@
 import { Rating } from "@smastrom/react-rating";
 import { Link } from "react-router-dom";
 const AdvertiseCard = ({ add }) => {
-  const { id, name, description, image, ratings } = add;
+  const { id, company, advertisement,  } = add;
 
   return (
     <div>
       <div className="card bg-base-100 shadow-xl border border-b-0 border-t-orange-200 border-r-orange-100 border-l-orange-100">
         <figure>
-          <img src={image} alt="Shoes" />
+          <img src={company.logo_url} alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title font-bold text-2xl">{name}</h2>
-          <p className="py-2 text-justify">{description}</p>
+          <h2 className="card-title font-bold text-2xl">{company.name}</h2>
+          <p className="py-2 text-justify">{advertisement.description}</p>
           <div className="card-actions justify-between items-center">
             <h4 className="font-semibold">
               <Rating
                 className="text-yellow-400"
                 style={{ maxWidth: 100 }}
-                value={ratings}
+                value={advertisement.reviews[0].rating}
                 readOnly
               />
             </h4>
